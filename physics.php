7f7b5025d79a51557646acea77a6ba6adf2d5f9b
novmoullina <52420747+novmoullina@users.noreply.github.com>
2019-07-13 16:40:53 +0700

    Update physics.php
    membuat rumus suhu

--- physics.php
+++ physics.php
@@ -2,7 +2,14 @@
 include "header.php";
 ?>
 <div class="hero">
-	
+	<h2>Besaran Satuan</h2>
+	<p>Suhu adalah besaran yang menyatakan derajat
+panas dan dingin suatu benda.</p>
+<p>1. Alat Ukur Suhu</p>
+<p>Alat untuk mengukur suhu adalah termometer.
+Ada 2 macam termometer, yaitu termo-meter
+berisi alkohol dan air raksa</p>
+<img src="suhu.png">
 </div>
 <?php
 include "footer.php";
