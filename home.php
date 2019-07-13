d3a9453c2f1ca89610f94b27e1a63119b0311bc6
novmoullina <52420747+novmoullina@users.noreply.github.com>
2019-07-13 12:20:19 +0700

    Update home.php
    nljhj

--- home.php
+++ home.php
@@ -4,6 +4,9 @@ include "header.php";
 		<div class="hero">
 			<h3 style="padding-left: 20px">Web untuk yang kurang kerjaan atau yang lagi butuh hiburan semata wayang<br>
 				Silahkan tambah karangan indah kalian
+
+
+				fhof qfhf q
 			</h3>
 		</div>
 <?php
