cdce08f32439a854b12a6c549ba2ce7aa884c220
novmoullina <52420747+novmoullina@users.noreply.github.com>
2019-07-13 16:44:16 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+include "header.php";
+?>
+		<div class="hero">
+			<h3>Web untuk yang kurang kerjaan atau yang lagi butuh hiburan semata wayang<br>
+				asdfasdf
+			</h3>
+		</div>
+<?php
+include "footer.php";
+?>
