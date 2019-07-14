994e26edf219f0f831dbfa5cf3fb5e8c2ecb483e
Weslie Leonardo <weslie.leonardo10@gmail.com>
2019-07-14 11:01:07 +0700

    Update physics.php
    tambah dikit

--- physics.php
+++ physics.php
@@ -7,30 +7,30 @@ include "header.php";
 	dan dinyatakan dengan angka.</p>
 	<h4>1. Besaran Pokok</h4>
 	<p>Besaran pokok adalah besaran yang satuannya telah ditetapkan terlebih dahulu dan tidak diturunkan dari besaran lain.</p>
-	<img src="img/besranpokok.PNG">
+	<img src="img/besaranpokok.PNG" alt="besaranpokok">
 	<h4>2. Besaran Turunan</h4>
 	<p>Besaran turunan adalah besaran yang diturunkan dari satu atau lebih besaran pokok</p>
-	<img src="img/besaranturunan.jpg">
+	<img src="img/besaranturunan.jpg" alt="besaranturunan">
 	<hr>
 
 	<h3>Zat dan Wujudnya</h3>
 	<h4>A. Wujud Zat</h4>
-	<img src="img/zatwujud.PNG">
+	<img src="img/zatwujud.PNG" alt="zatwujud">
 	<h4>B. Perubahan Wujud Zat</h4>
 	<h5>1. Perubahan Fisika</h5>
 	<p>Perubahan fisika adalah perubahan zat yang tidak menyebabkan terjadinya zat jenis baru. Contoh : es mencair, air menjadi uap.</p>
-	<img src="img/fisika.PNG">
+	<img src="img/fisika.PNG" alt="perubahanfisika" width="300px" height="150px">
 	<h5>2. Perubahan Kimia</h5>
 	<p>Perubahan kimia adalah perubahan zat yang menyebabkan terjadinya zat baru. Contoh : kayu terbakar menghasilkan api, arang, dan debu.</p>
 	<h4>Massa Jenis</h4>
 	<h5>1. Massa Jenis</h5>
 	<p>Massa jenis suatu zat adalah massa per satuan volume zat tersebut</p>
-	<img src="img/mj.PNG">
+	<img src="img/mj.PNG" alt="mj">
 	<h5>2. Massa Jenis Relatif</h5>
 	<p>Massa jenis relatif adlah prbandingan massa jenis suatu benda dengan massa jenis air</p>
-	<img src="img/mjr.PNG">
+	<img src="img/mjr.PNG" alt="mjr">
 	<h5>3. Massa Jenis Campuran</h5>
-	<img src="img/mjc.PNG">
+	<img src="img/mjc.PNG" alt="mjc">
 </div>
 <?php
 include "footer.php";
