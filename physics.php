3b1ff5987cb01c6611b3136e63c67b845c9204e4
novmoullina <52420747+novmoullina@users.noreply.github.com>
2019-07-14 10:24:23 +0700

    physics.php
    menambah materi

--- physics.php
+++ physics.php
@@ -2,14 +2,26 @@
 include "header.php";
 ?>
 <div class="hero">
-	<h2>Besaran Satuan</h2>
-	<p>Suhu adalah besaran yang menyatakan derajat
-panas dan dingin suatu benda.</p>
-<p>1. Alat Ukur Suhu</p>
-<p>Alat untuk mengukur suhu adalah termometer.
-Ada 2 macam termometer, yaitu termo-meter
-berisi alkohol dan air raksa</p>
-<img src="img/suhu.png">
+	<h3>Besaran Satuan</h3>
+	<p>Besaran adalah segala sesuatu yang dapat diukur
+	dan dinyatakan dengan angka.</p>
+	<h4>1. Besaran Pokok</h4>
+	<p>Besaran pokok adalah besaran yang satuannya telah ditetapkan terlebih dahulu dan tidak diturunkan dari besaran lain.</p>
+	<img src="img/besranpokok.PNG">
+	<h4>2. Besaran Turunan</h4>
+	<p>Besaran turunan adalah besaran yang diturunkan dari satu atau lebih besaran pokok</p>
+	<img src="img/besaranturunan.jpg">
+	<hr>
+
+	<h3>Zat dan Wujudnya</h3>
+	<h4>A. Wujud Zat</h4>
+	<img src="img/zatwujud.PNG">
+	<h4>B. Perubahan Wjud Zat</h4>
+	<h5>1. Perubahan Fisika</h5>
+	<p>Perubahan fisika adalah perubahan zat yang tidak menyebabkan terjadinya zat jenis baru. Contoh : es mencair, air menjadi uap.</p>
+	<img src="img/fisika.PNG">
+	<h5>2. Perubahan Kimia</h5>
+	<p>Perubahan kimia adalah perubahan zat yang menyebabkan terjadinya zat baru. Contoh : kayu terbakar menghasilkan api, arang, dan debu.</p>
 </div>
 <?php
 include "footer.php";
