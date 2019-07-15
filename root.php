2f27ffc5f2d0fec6d5629022feef4da106c3559f
novmoullina <52420747+novmoullina@users.noreply.github.com>
2019-07-15 19:29:58 +0700

    Update root.php
    menghitung pangkat

--- root.php
+++ root.php
@@ -2,7 +2,42 @@
 include "header.php";
 ?>
 <div class="hero">
-	
+<?php
+@$bilangan = $_POST['bilangan'];
+@$pangkat = $_POST['pangkat'];
+@$hasil = pow($bilangan, $pangkat);
+?>
+
+
+        <form method="POST">
+            <table border="1" align="center" >
+                <tr>
+                    <td>Input Bilangan</td>
+                    <td>=</td>
+                    <td><input name="bilangan" type="text" value="<?php echo $bilangan; ?>"/></td>
+                </tr>
+                <tr>
+                    <td>Pangkat</td>
+                    <td>=</td>
+                    <td><input name="pangkat" type="text" value="<?php echo $pangkat; ?>"/></td>
+                </tr>
+                <tr>
+                    <td><input name="submit" type="submit" value="SUBMIT"/></td>
+                </tr>
+                <tr>
+                    <td>
+                        <?php
+                        if(isset ($bilangan))
+                            echo $hasil;
+                        ?>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
+
+<hr>	
 </div>
 <?php
 include "footer.php";
