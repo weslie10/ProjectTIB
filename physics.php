d93d2d44b20b0d0f881ecb48a98c5ac8335f6924
novmoullina <52420747+novmoullina@users.noreply.github.com>
2019-07-15 17:45:34 +0700

    edit physics.php

--- physics.php
+++ physics.php
@@ -23,36 +23,36 @@ include "header.php";
 	<h5>2. Perubahan Kimia</h5>
 	<p>Perubahan kimia adalah perubahan zat yang menyebabkan terjadinya zat baru. Contoh : kayu terbakar menghasilkan api, arang, dan debu.</p>
 
-	<h4>Massa Jenis</h4>
-	<h5>1. Massa Jenis</h5>
+	<h3>Massa Jenis</h3>
+	<h4>1. Massa Jenis</h4>
 	<p>Massa jenis suatu zat adalah massa per satuan volume zat tersebut</p>
 	<img src="img/mj.PNG" alt="mj">
-	<h5>2. Massa Jenis Relatif</h5>
+	<h4>2. Massa Jenis Relatif</h4>
 	<p>Massa jenis relatif adlah prbandingan massa jenis suatu benda dengan massa jenis air</p>
 	<img src="img/mjr.PNG" alt="mjr">
 	<h5>3. Massa Jenis Campuran</h5>
 	<img src="img/mjc.PNG" alt="mjc">
 	<img src="img/keterangan.PNG" alt="keterangan">
 
-	<h4>Gerak</h4>
-	<h5>A. Pengertian Gerak</h5>
+	<h3>Gerak</h3>
+	<h4>A. Pengertian Gerak</h4>
 	<p>Gerak adalah perubahan kedudukan suatu benda
 	terhadap titik acuannya.</p>
-	<p>Gerak menurut keadaan benda:
-		- gerak yang sebenarnya,
-		- gerak semu.
-		Gerak menurut bentuk lintasan:
-		- gerak lurus,
-		- gerak melingkar,
-		- gerak parabola,
-	- gerak tidak beraturan.</p>
-	<h5>B. Gerak Lurus Beraturan</h5>
+	<p>Gerak menurut keadaan benda:</p>
+	<p>- gerak yang sebenarnya</p>
+	<p>- gerak semu.</p>
+	<p>Gerak menurut bentuk lintasan:</p>
+	<p>- gerak lurus</p>
+	<p>- gerak melingkar</p>
+	<p>- gerak parabola</p>
+	<p>- gerak tidak beraturan.</p>
+	<h4>B. Gerak Lurus Beraturan</h4>
 	<p>Gerak lurus beraturan: gerak suatu benda yang
 		lintasannya berupa garis lurus dan besar kecepatannya
 	setiap saat selalu sama atau tetap.</p>
-	<p>1. Kecepatan Tetap</p>
+	<h5>1. Kecepatan Tetap</h5>
 	<img src="img/ktetap.PNG" alt="ktetap">
-	<p>2. Kecepatan Rata-rata</p>
+	<h5>2. Kecepatan Rata-rata</h5>
 	<img src="img/krata.PNG" alt="krata">
 	<h5>C. Gerak Lurus Berubah Beraturan (GLBB)</h5>
 	<p>Gerak lurus berubah beraturan: gerak lurus yang
@@ -63,8 +63,8 @@ include "header.php";
 	adalah percepatan yang bernilai negatif.</p>
 	<img src="img/glbb.PNG" alt="glbb">
 
-	<h4>Gaya</h4>
-	<h5>A. Pengertian Gaya</h5>
+	<h3>Gaya</h3>
+	<h4>A. Pengertian Gaya</h4>
 	<p>Gaya adalah sesuatu yang dapat menyebabkan
 		terjadinya perubahan kecepatan dan perubahan
 		bentuk suatu benda. Alat untuk mengukur gaya
@@ -74,26 +74,26 @@ include "header.php";
 	<p>Gaya sentuh adalah gaya yang mempengaruhi benda
 		dengan cara bersentuhan langsung dengan
 	benda tersebut. </p>
-	<br>Contoh: gaya otot.
+	<p>Contoh: gaya otot.</p>
 	<p>2. Gaya Tak Sentuh</p>
 	<p>Gaya tak sentuh adalah gaya yang mempengaruhi benda
 		tanpa bersentuhan langsung dengan benda
 	tersebut. </p>
-	<br>Contoh: gaya magnet, gaya listrik.
-	<h5>B. Resultan Gaya</h5>
+	<p>Contoh: gaya magnet, gaya listrik.</p>
+	<h4>B. Resultan Gaya</h4>
 	<p>Gaya yang arahnya sama dapat diganti dengan
 		sebuah gaya yang nilainya sama dengan jumlah
 		kedua gaya. Gaya pengganti itu disebut resultan
 	gaya yang dilambangkan dengan R.</p>
 	<img src="resultangaya.PNG" alt="resultangaya">
-	<h5>C. Gaya Berat dan Berat Jenis Benda</h5>
+	<h4>C. Gaya Berat dan Berat Jenis Benda</h4>
 	<p>Gaya berat adalah besaran gaya gravitasi yang
 		bekerja pada benda. Hubungan antara massa
-		benda dan beratnya yaitu: </p>
-		<br>1. semakin besar massa benda, semakin besar
-		pula beratnya,
-		<br>2. perbandingan antara berat benda dan massa
-	benda cenderung tetap.
+	benda dan beratnya yaitu: </p>
+	<p>1. semakin besar massa benda, semakin besar
+	pula beratnya,</p>
+	<p>2. perbandingan antara berat benda dan massa
+	benda cenderung tetap.</p>
 	<img src="img/gayaberat.PNG" alt="gayaberat">
 
 </div>
